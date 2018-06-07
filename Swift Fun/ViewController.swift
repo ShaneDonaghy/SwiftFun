fda6246aca6460277a6d7c5144ca5fc7925727d7
Shane Donaghy <donaghys@tcd.ie>
2018-06-07 19:11:37 +0100

    Initial Commit

---
+++ Swift Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Fun
+//
+//  Created by Shane Donaghy on 07/06/2018.
+//  Copyright © 2018 Shane Donaghy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
