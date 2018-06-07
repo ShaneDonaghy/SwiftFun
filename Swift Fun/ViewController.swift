1630fb0a9bee5e69af83ed5e66dc0fb5e2cf51b5
Shane Donaghy <donaghys@tcd.ie>
2018-06-07 20:45:50 +0100

    My first working version of the bare bones of the app
    Major functionality here is the tap button changing b/g colour and label text

--- Swift Fun/ViewController.swift
+++ Swift Fun/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonCount = 0
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
+    @IBAction func changeText(_ sender: UIButton) {
+        
+        buttonCount += 1
+        
+        if(buttonCount >= 10 && buttonCount < 15){
+            view.backgroundColor = UIColor.red
+            myLabel.text = "You've tapped me \(buttonCount) times: Im telling HR"
+        } else if (buttonCount > 15){
+            view.backgroundColor = UIColor.green
+            myLabel.text = "Kiss me, you fool"
+        }
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
